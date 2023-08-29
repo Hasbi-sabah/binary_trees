818d923f477660eaa3fb447f39ee93841ace8ef2
H. Sabah <sabahhasbi00@gmail.com>
2023-08-29 10:04:10 +0000

    if func is null

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -78,7 +78,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	queue_t *queue;
 	binary_tree_t *current;
 
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
 	queue = createQueue();
