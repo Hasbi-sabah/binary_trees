bad47a74ec9794df5a37888f5fa27a6cbf146df1
elfadili-ae <elfadili.ae@gmail.com>
2023-08-29 11:06:43 +0100

    Style: betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,15 +43,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t size = 0;
+	size_t size = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        size++;
-        size += binary_tree_size(tree->left);
-        size += binary_tree_size(tree->right);
-        return (size);
+	size++;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	return (size);
 }
 
 /**
@@ -61,7 +61,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        int size, height;
+	int size, height;
 
 	if (tree == NULL)
 		return (0);
