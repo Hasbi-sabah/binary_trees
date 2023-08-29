4f8891269228c567113885a9a7c6316128fe4cce
H. Sabah <sabahhasbi00@gmail.com>
2023-08-29 07:08:44 +0000

    compilation fix in 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,7 @@
  * @tree: tree
  * @func: printing func
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
