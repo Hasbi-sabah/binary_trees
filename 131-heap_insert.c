51ca7b3100f45bbc5b274688010d651c072a8d37
H. Sabah <sabahhasbi00@gmail.com>
2023-08-31 14:19:32 +0000

    Files added: task 37 incomplete

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,36 +2,41 @@
 
 heap_t *heap_search(heap_t **root, int value, int *flag)
 {
-	if (value > tree->n)
+	if (value > (*root)->n)
 	{
-		return (NULL);
+		flag = 0;
+		return ((*root)->parent);
 	}
 
-	if (tree->left)
-	{
-		if (value >= tree->left->n)
-		{
-			*flag = -1;
-			return (*root);
-		}
-		is_heap(tree->left);
-	}
-	if (tree->right)
-	{
-		if (value >= tree->right->n)
-		{
-			*flag = 1;
-			return (*root);
-		}
-		is_heap(tree->right);
-	}
-	if (!tree->left)
+	if (!(*root)->left)
 	{
 		*flag = -1;
 		return (*root);
 	}
-	*flag = 1;
-	return (*root);
+	else if (!(*root)->right)
+	{
+		*flag = 1;
+		return (*root);
+	}
+	if ((*root)->left)
+	{
+		if (value >= (*root)->left->n)
+		{
+			*flag = -1;
+			return (*root);
+		}
+		heap_search(&(*root)->left, value, flag);
+	}
+	if ((*root)->right)
+	{
+		if (value >= (*root)->right->n)
+		{
+			*flag = 1;
+			return (*root);
+		}
+		heap_search(&(*root)->right, value, flag);
+	}
+	return (NULL);
 }
 heap_t *heap_insert(heap_t **root, int value)
 {
@@ -41,16 +46,38 @@ heap_t *heap_insert(heap_t **root, int value)
 	if (!*root)
 	{
 		node = binary_tree_node(NULL, value);
-		return (*root = node);
+		*root = node;
+		return (node);
 	}
 	pa = heap_search(root, value, &flag);
 	node = binary_tree_node(pa, value);
+	if (flag == 0)
+	{
+		node->left = *root;
+		(*root)->parent = node;
+		(*root) = node;
+	}
 	if (flag == - 1)
 	{
 		if (pa->left)
 		{
-
+			pa->left->parent = node;
+			node->left = pa->left; 
+			pa->left = node;
 		}
-		pa->left = node;
+		else
+			pa->left = node;
 	}
+	if (flag == 1)
+	{
+		if (pa->right)
+		{
+			pa->right->parent = node;
+			node->right = pa->right; 
+			pa->right = node;
+		}
+		else
+			pa->right = node;
+	}
+	return (node);
 }
